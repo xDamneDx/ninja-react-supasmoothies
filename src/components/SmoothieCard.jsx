94c5f6c034ed4f6437891b748793db53bd9f6158
xDamneDx <xdamnedx@gmail.com>
2023-03-29 14:55:00 +0300

    fetching data

---
+++ src/components/SmoothieCard.jsx
@@ -0,0 +1,9 @@
+export default function SmoothieCard({ smoothie }) {
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      <p>{smoothie.method}</p>
+      <div className="rating">{smoothie.rating}</div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
+import { useState, useEffect } from "react";
 import { supabase } from "../config/supabaseClient";
 
+// Components:
+import SmoothieCard from "../components/SmoothieCard";
+
 export default function Home() {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies..");
+        setSmoothies(null);
+
+        return;
+      }
+
+      setSmoothies(data);
+      setFetchError(null);
+    };
+
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }

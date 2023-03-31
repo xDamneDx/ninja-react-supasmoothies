26443cbde61654eccd668a1fd815db1ed6e3fc95
xDamneDx <xdamnedx@gmail.com>
2023-03-31 11:24:50 +0300

    deleting records

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
+import { supabase } from "../config/supabaseClient";
 
 export default function SmoothieCard({ smoothie }) {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("smoothies")
+      .delete()
+      .eq("id", smoothie.id)
+      .select();
+
+    if (error) {
+      console.log({ error });
+    }
+
+    console.log({ data });
+  };
+
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -10,6 +25,9 @@ export default function SmoothieCard({ smoothie }) {
         <Link to={`/${smoothie.id}`}>
           <i className="material-icons">edit</i>
         </Link>
+        <i onClick={handleDelete} className="material-icons">
+          delete
+        </i>
       </div>
     </div>
   );
